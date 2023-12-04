c42d7f57a71b02556dae2a409bc9fc53e6451b5e
Adil Nadeem <adilnadeem709@gmail.com>
2023-12-04 22:52:33 +0500

    completed basic A 3 and 4

---
+++ A/4_144.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i, j;
+    int n;
+    // int count = 0;
+
+    do
+    {
+        cin >> n;
+    } while (n < 0 || n > 100);
+
+    for (i = 1; i <= n; i++)
+    {
+        /* code */
+    }
+    
+
+   return 0;
+}
