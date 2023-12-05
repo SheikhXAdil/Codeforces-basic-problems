1e747cad7b4f96be6a27fbaa4812cff916359ecd
Adil Nadeem <adilnadeem709@gmail.com>
2023-12-05 21:12:33 +0500

    completed B 5

--- B/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-for (j = position; j > position - countOf1; j--)
-            // {
-            //     s[j] = '2';
-            //     if (turnCount % 2 == 1)
-            //         alicePoints++;
-            // }
