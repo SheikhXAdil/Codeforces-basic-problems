81f752330394c6454cd67c107081c868a8f0636a
Adil Nadeem <adilnadeem709@gmail.com>
2023-12-11 20:35:31 +0500

    added A 4 and A 5

--- A/4_144.cpp
+++ A/4_144.cpp
@@ -13,11 +13,15 @@ int main()
         cin >> n;
     } while (n < 0 || n > 100);
 
-    for (i = 1; i <= n; i++)
+    if (n % 2 == 1)
+        printf("-1");
+    else
     {
-        /* code */
+        for (i = 1; i <= n / 2; i++)
+        {
+            printf("%d %d ", 2 * i, 2 * (i - 1) + 1);
+        }
     }
-    
 
-   return 0;
+    return 0;
 }
